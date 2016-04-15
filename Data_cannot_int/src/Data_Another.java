06bb8b491a9adda25190bd42705f90c2934eb5ff
iittuski <iittuski@gmail.com>
2016-04-14 21:15:27 -0700

    Assignment
    Assignment

---
+++ Data_cannot_int/src/Data_Another.java
@@ -0,0 +1,9 @@
+
+public class Data_Another {
+
+	public static void main(String args[]) {
+		/// The Data That Cannot be Stored in type int is Double and Long
+		int i = 1.24;
+		int j = 32165165165131568135168163516816185;
+	}
+}
